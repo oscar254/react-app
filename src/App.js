1bf9df19005e89876e231dbd1e94868ab28079b9
oscar254 <40966731+oscar254@users.noreply.github.com>
2019-03-16 09:16:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>
+            Change by Oscar254
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
